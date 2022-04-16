69fa3eda145da2531dbd7da26f3eab62ac4dbaf2
arzuakhundlu <akhundzada.arzu@gmail.com>
2022-04-16 23:18:12 +0400

    Change sort

---
+++ JS/main.js
@@ -0,0 +1,113 @@
+let inp = document.querySelector('.toDoInp');
+let toDoText = document.getElementsByClassName('todolist-text');
+let btn = document.getElementById('list-add');
+let warning = document.querySelector('.p p');
+let div = document.querySelector('.list-box');
+let ul = document.querySelector('.todolist-task');
+
+
+btn.addEventListener('click',()=>{
+    
+    let li = document.createElement('li');
+    let span1 = document.createElement('span');
+    span1.className = 'inp-text';
+    let span2 = document.createElement('span')
+    span2.className = 'delete';
+    let icon = document.createElement('i');
+    icon.setAttribute('class','fa-regular fa-circle-xmark');
+    
+    let spanIcon = document.createElement('span')
+    spanIcon.className = 'icons';
+    let spanEdit = document.createElement('span');
+    spanEdit.className ='edit'
+    let iconEdit = document.createElement('i');
+    iconEdit.setAttribute('class','fa-solid fa-square-pen');
+    
+    
+    if(inp.value === ''){
+        warning.style.display = 'block';
+    }
+    else{
+        warning.style.display = 'none';
+        span1.innerHTML = inp.value;
+        div.style.display = 'block';
+        ul.append(li);
+        li.append(span1);
+        li.append(spanIcon);
+        spanIcon.append(spanEdit);
+        spanEdit.append(iconEdit)
+        spanIcon.append(span2)
+        span2.append(icon);
+        inp.value = '';
+    }
+    
+    // remove li
+    span2.addEventListener('click',() => {
+        ul.removeChild(li); 
+        if(ul.innerHTML === ''){
+            div.style.display = 'none';
+        }
+    });
+    
+    // edit
+    let clickCounter = 0;
+    spanEdit.addEventListener('click',()=> {
+        
+        clickCounter++;
+        if(clickCounter === 1){
+            span1.contentEditable = true;
+            span1.style.backgroundColor = '#dbdbdb'
+        }
+        else{
+            span1.contentEditable = false;
+            span1.style.backgroundColor = '#fff'
+            
+        }
+        
+    });
+    
+    
+    // sort
+
+    let arrow = document.querySelector('.arrow');
+    let clickArrow = 0;
+    arrow.addEventListener('click', () => {
+        clickArrow++;
+        let span1All = document.querySelectorAll('.inp-text');
+        let arr = [];
+        span1All.forEach(span1 =>{
+            arr.push(span1.innerText)
+        });
+        if(clickArrow === 1){
+            arr.sort();  
+            arr.forEach((item,i)=>{
+                span1All[i].innerText = item;
+            })
+        }
+        else{
+            arr.sort().reverse();  
+            arr.forEach((item,i)=>{
+                span1All[i].innerText = item;
+            })
+        }
+    });
+});
+
+
+// keyup Enter
+inp.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     document.getElementById('list-add').click();
+    }
+  });
+
+
+
+// input default
+let xIcon = document.querySelector('.xIcon');
+xIcon.addEventListener('click',()=>{
+    inp.value = '';
+});
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# To-do-list
+
+# Link -- https://modul2-todolist.netlify.app/
+
+
+## Validator
+![to-do-list-checker](https://user-images.githubusercontent.com/74364143/163684928-f209bb11-7b0e-439a-b8e0-412871b91eea.png)
